9089365409d14d7b7f71d0e1a2a7fcf716dbcb52
bimal <90305421+bimal-gajera@users.noreply.github.com>
2024-02-17 22:36:16 -0700

    Added UI and single round game play

--- script.js
+++ script.js
@@ -25,31 +25,77 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Enter you choice: ");
-    let computerSelection = getComputerChoice();
-    let roundOutcome = playRound(playerSelection, computerSelection);
-    console.log(roundOutcome);
+// function playGame() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   for (let i = 0; i < 5; i++) {
+//     let playerSelection = prompt("Enter you choice: ");
+//     let computerSelection = getComputerChoice();
+//     let roundOutcome = playRound(playerSelection, computerSelection);
+//     console.log(roundOutcome);
 
-    let result = roundOutcome.split('!');
-    if (result[0] == "You Win") {
-      playerScore++;
-    }
+//     let result = roundOutcome.split('!');
+//     if (result[0] == "You Win") {
+//       playerScore++;
+//     }
 
-    else if (result[0] == "You Lose") {
-      computerScore++;
-    }
-  }
-  if (playerScore > computerScore) {
-    return "Player wins!";
-  }
-  else if (playerScore < computerScore) {
-    return "Computer wins!";
-  }
-  return "Tie Game!"
+//     else if (result[0] == "You Lose") {
+//       computerScore++;
+//     }
+//   }
+//   if (playerScore > computerScore) {
+//     return "Player wins!";
+//   }
+//   else if (playerScore < computerScore) {
+//     return "Computer wins!";
+//   }
+//   return "Tie Game!"
+// }
+
+// console.log(playGame());
+
+// let playerSelection = prompt("Enter you choice: ");
+// let computerSelection = getComputerChoice();
+// console.log(alert(playRound(playerSelection, computerSelection)));
+
+let myDiv = document.querySelector('#mainDiv');
+let result;
+
+let outputDiv = document.createElement('h3');
+
+function createButton(buttonText) {
+  let button = document.createElement('BUTTON');
+  let text = document.createTextNode(buttonText);
+  button.appendChild(text);
+  button.style.cssText = "color:white";
+  button.style.margin = "10px";
+  button.style.padding = "10px";
+  button.style.backgroundColor = "black";
+  button.id = buttonText;
+
+  myDiv.appendChild(button);
 }
 
-console.log(playGame());
+createButton("Rock");
+createButton("Paper");
+createButton("Scissor");
+
+myDiv.appendChild(outputDiv);
+
+document.getElementById("Rock").addEventListener("click", () => {
+  result = playRound("Rock", getComputerChoice());
+  outputDiv.textContent = result;
+})
+
+document.getElementById("Paper").addEventListener("click", () => {
+  result = playRound("Paper", getComputerChoice());
+  outputDiv.textContent = result;
+})
+
+document.getElementById("Scissor").addEventListener("click", () => {
+  result = playRound("Scissor", getComputerChoice());
+  outputDiv.textContent = result;
+})
+
+
+
